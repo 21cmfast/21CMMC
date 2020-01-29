48766fc9bd8efc420dccfdc7d7334958d1f844ad
Steven Murray <steven.murray@curtin.edu.au>
2020-01-28 18:46:17 -0700

    Made luminosity function core/likelihood better at reading data

--- src/py21cmmc/core.py
+++ src/py21cmmc/core.py
@@ -550,8 +550,10 @@ class CoreLuminosityFunction(CoreCoevalModule):
     A Core Module that produces model luminosity functions at a range of redshifts.
     """
 
-    def __init__(self, sigma, **kwargs):
+    def __init__(self, sigma=None, name="", n_muv_bins=100, **kwargs):
         self._sigma = sigma
+        self.name = str(name)
+        self.n_muv_bins = n_muv_bins
         super().__init__(**kwargs)
 
     def setup(self):
@@ -569,6 +571,7 @@ class CoreLuminosityFunction(CoreCoevalModule):
             flag_options=self.flag_options,
             cosmo_params=cosmo_params,
             user_params=self.user_params,
+            nbins=self.n_muv_bins,
         )
 
     def build_model_data(self, ctx):
@@ -582,10 +585,16 @@ class CoreLuminosityFunction(CoreCoevalModule):
         mhalo = [m[~np.isnan(l)] for l, m in zip(lfunc, mhalo)]
         lfunc = [m[~np.isnan(l)] for l, m in zip(lfunc, lfunc)]
 
-        ctx.add("luminosity_function", {"Muv": Muv, "mhalo": mhalo, "lfunc": lfunc})
+        ctx.add(
+            "luminosity_function" + self.name,
+            {"Muv": Muv, "mhalo": mhalo, "lfunc": lfunc},
+        )
 
     @property
     def sigma(self):
+        if self._sigma is None:
+            return None
+
         if not hasattr(self._sigma, "__len__") or len(self._sigma) != len(
             self.redshift
         ):
@@ -594,11 +603,15 @@ class CoreLuminosityFunction(CoreCoevalModule):
             return self._sigma
 
     def convert_model_to_mock(self, ctx):
-        lfunc = ctx.get("luminosity_function")["lfunc"]
-        muv = ctx.get("luminosity_function")["Muv"]
+        if self.sigma is None:
+            raise ValueError("Cannot create a mock with sigma=None!")
+
+        lfunc = ctx.get("luminosity_function" + self.name)["lfunc"]
+        muv = ctx.get("luminosity_function" + self.name)["Muv"]
 
         for i, s in enumerate(self.sigma):  # each redshift
             try:
                 lfunc[i] += np.random.normal(loc=0, scale=s(muv), size=len(lfunc[i]))
             except TypeError:
+
                 lfunc[i] += np.random.normal(loc=0, scale=s, size=len(lfunc[i]))
