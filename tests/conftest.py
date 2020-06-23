016d8717a5dbd428c859868f9d8c5cd2595b1632
Steven Murray <steven.g.murray@asu.edu>
2020-06-22 20:13:47 -0700

    test: raise error if external_table_path doesn't exist

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,19 @@
+import os
+
 import pytest
+
+from py21cmfast import global_params
 
 
 @pytest.fixture(scope="session")
 def tmpdirec(tmpdir_factory):
     return tmpdir_factory.mktemp("data")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def setup_package():
+    txt = "".join(a.decode() for a in global_params.external_table_path)
+    txt.replace(r"\x00", "")
+    print("External table path: ", txt)
+    if not os.path.exists(txt):
+        raise IOError("External table path does not exist!")
