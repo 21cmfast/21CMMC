23a32eee836e3dcfb1e6a38f8cb8eeafab359105
Yuxiang Qin <Yuxiang.L.Qin@gmail.com>
2020-06-24 15:58:00 +1000

    wbt now?

--- pre_install.sh
+++ pre_install.sh
@@ -1,4 +1,6 @@
 git clone https://github.com/JohannesBuchner/MultiNest
+cmake MultiNest
+make -c MultiNest/build/
 cd MultiNest/build && cmake .. && make && cd ../lib/
 export LD_LIBRARY_PATH="`pwd`:$LD_LIBRARY_PATH"
 cd ../..
