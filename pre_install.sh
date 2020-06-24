ddc31f39d614d4ca42fa354bbaecd8eb6bc5abed
Yuxiang Qin <Yuxiang.L.Qin@gmail.com>
2020-06-24 18:55:10 +1000

    seem cannot change LD_LIBRARY_PATH in tox

--- pre_install.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/JohannesBuchner/MultiNest
-cmake MultiNest
-make -c MultiNest/build/
-cd MultiNest/build && cmake .. && make && cd ../lib/
-export LD_LIBRARY_PATH="`pwd`:$LD_LIBRARY_PATH"
-cd ../..
