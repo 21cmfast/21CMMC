57ad1364a997d9f184dcd50868bda5ba6db88435
Yuxiang Qin <Yuxiang.L.Qin@gmail.com>
2020-06-25 11:43:39 +1000

    update test

--- tests/test_planck_lk.py
+++ tests/test_planck_lk.py
@@ -12,7 +12,7 @@ from py21cmmc import LikelihoodPlanckPowerSpectra, CoreCMB
 def test_planck():
     chain = mcmc.run_mcmc(
     CoreCMB(),
-    LikelihoodPlanckPowerSpectra(datafolder = ",/baseline/plc_3.0/low_l/simall/simall_100x143_offlike5_EE_Aplanck_B.clik/",name_lkl = 'Planck_lowl_EE'),
+    LikelihoodPlanckPowerSpectra(name_lkl = 'Planck_lowl_EE'),
     params=dict(             # Parameter dict as described above.
         F_STAR10 = [-1.3, -3, 0, 1.0],
         ALPHA_STAR = [0.5, -0.5, 1.0, 1.0],
