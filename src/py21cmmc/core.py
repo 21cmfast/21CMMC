wrong index

src/py21cmmc/core.py
@@ -901,8 +901,8 @@ class CoreForest(CoreLightConeModule):
         if self.observation == "xqr30":
             index = np.where(np.asarray(lc.node_redshifts) < self.redshift[0])[0][0]
             filling_factor = lc.global_xH[index] * (
-                lc.node_redshifts[index + 1] - self.redshift[0]
-            ) + lc.lobal_xH[index + 1] * (self.redshift[0] - lc.node_redshifts[index])
+                lc.node_redshifts[index - 1] - self.redshift[0]
+            ) + lc.lobal_xH[index - 1] * (self.redshift[0] - lc.node_redshifts[index])
             ctx.add("filling_factor_%s" % self.name, filling_factor)
             if not self.mean_flux:
                 if filling_factor > 0.7: