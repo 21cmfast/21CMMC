42c6ab4e9368ef9d574ac303181c2047684bb2ec
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2023-07-17 12:18:51 +0200

    Check tf install

--- tests/test_emulator.py
+++ tests/test_emulator.py
@@ -1,9 +1,9 @@
-import numpy as np
-
 def test_emulator_install():
     from py21cmemu import Emulator
+    print(tf.random.normal(10))
     emu = Emulator()
 
 def test_emulator_runs():
     from py21cmemu import Emulator
+    import numpy as np
     check = Emulator().predict(np.random.rand(9))
