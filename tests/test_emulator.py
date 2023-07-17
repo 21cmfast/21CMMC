0889328faf776e2e52084936fc0f3a8492000837
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2023-07-17 13:37:10 +0200

    Check emu download

--- tests/test_emulator.py
+++ tests/test_emulator.py
@@ -2,6 +2,10 @@ def test_emulator_install():
     from py21cmemu import Emulator
     from py21cmemu.get_emulator import get_emu_data
     get_emu_data()
+
+    import tensorflow as tf
+    from py21cmemu.config import CONFIG
+    m = tf.keras.load_model(CONFIG.data_path / '21cmEMU')
     #emu = Emulator()
 
 def test_emulator_runs():
