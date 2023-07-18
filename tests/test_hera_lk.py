5adb8bdadd513b3bf971e7574df56232619026da
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2023-07-18 11:32:57 +0200

    Adding changes from h1c branch

---
+++ tests/test_hera_lk.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from py21cmmc import (
+    Likelihood1DPowerLightconeUpper,
+    LikelihoodPlanck,
+    analyse,
+    build_computation_chain,
+    core,
+    likelihood,
+    mcmc,
+    run_mcmc,
+)
+
+
+def test_hera_lk():
+
+    lk = Likelihood1DPowerLightconeUpper.from_builtin_data("HERA_H1C_IDR3")
+
+    c21cmemu = core.Core21cmEMU()
+    chain = build_computation_chain(
+        [c21cmemu],
+        [lk],
+        setup=True,
+    )
+    chain({})
