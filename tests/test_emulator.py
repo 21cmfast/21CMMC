ce71b75759c9bc7ddc7bf4ba2abe42258035fb51
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2023-07-19 15:20:53 +0200

    Change test

--- tests/test_emulator.py
+++ tests/test_emulator.py
@@ -1,8 +1,5 @@
-def test_emulator_install():
-    from py21cmemu import Emulator
-    emu = Emulator()
-
 def test_emulator_runs():
     from py21cmemu import Emulator
     import numpy as np
-    check = Emulator().predict(np.random.rand(9))
+    emu = Emulator()
+    emu.predict(np.random.rand(9))
