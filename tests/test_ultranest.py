813fcf8e89d8aa510754bf6cce53a2ca34f8438e
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2023-09-01 15:51:09 +0200

    Typo

--- tests/test_ultranest.py
+++ tests/test_ultranest.py
@@ -1,8 +1,7 @@
 import pytest
 
 import numpy as np
-import ultranest
-from py21cmfast.utils import ParameterError
+from py21cmfast. utils import ParameterError
 
 import py21cmmc as mcmc
 from py21cmmc.cosmoHammer import Params
