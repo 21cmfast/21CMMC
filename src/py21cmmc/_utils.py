44d40cd5f02d5f689118abb3e85aa43d0861f968
Steven Murray <steven.g.murray@asu.edu>
2023-09-20 11:53:54 +0200

    fix: always use collections.abc.Iterable

--- src/py21cmmc/_utils.py
+++ src/py21cmmc/_utils.py
@@ -1,10 +1,6 @@
 """Utility functions used throughout 21CMMC."""
 
-try:
-    from collections.abc import Iterable  # Python <= 3.9
-
-except ImportError:
-    from collections.abc import Iterable  # Python > 3.9
+from collections.abc import Iterable
 
 
 def flatten(items):
